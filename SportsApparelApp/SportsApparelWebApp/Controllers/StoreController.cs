da47824c4011c17169c19d0ee2cf9da926ef018e
Alfred <arwagaju@gmail.com>
2020-07-04 19:10:57 -0400

    created a starting project for my web app

---
+++ SportsApparelApp/SportsApparelWebApp/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsApparelWebApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from Index";
+        }
+
+        public string Browse()
+        {
+            return "Hello from browse";
+        }
+
+        public string Details()
+        {
+            return "Hello from detials";
+        }
+
+
+    }
+}
