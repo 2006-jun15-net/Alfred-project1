d8aa66438588cc2163c891501d96fe5e4bc3585c
Alfred <arwagaju@gmail.com>
2020-07-11 03:04:36 -0400

    fixed a bug that prevented the price for a particular product to be displayed

--- SportsApparelApp/SportsApparelWebApp/Controllers/ShopController.cs
+++ SportsApparelApp/SportsApparelWebApp/Controllers/ShopController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApparelApp.Entities.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SportsApparelWebApp.Repositories;
 
+
 namespace SportsApparelWebApp.Controllers
 {
     public class ShopController : Controller
+
+
     {
+        private SportsApparelContext context;
+        public ShopController()
+        {
+             context = new SportsApparelContext();
+                
+        }
         public IActionResult Index()
         {
             CustomerRepo customerRepo = new CustomerRepo();
@@ -22,5 +32,18 @@ namespace SportsApparelWebApp.Controllers
 
             return View(objModel);
         }
+
+        [HttpGet]
+        public JsonResult GetPrice(int ProdID)
+
+        {
+            decimal price = (decimal)context.Product.Single(m => m.ProdId == ProdID).Price;
+            return Json(price);
+
+            
+
+        }
+
+
     }
 }
